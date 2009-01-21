425231daa707b6d1778a751fddc11abc1125a40b
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-21 23:59:34 +0000

    r1493@pythagoras (orig r1492):  mj0 | 2008-01-24 20:26:44 +0100  BUGFIX: KDM theme should work now for suse 10.2, others have still to be changed

--- trunk/os-plugins/plugins/Theme/XX_Theme.sh
+++ trunk/os-plugins/plugins/Theme/XX_Theme.sh
@@ -2,6 +2,7 @@
 #
 # stage3 part of 'Theme' plugin - the runlevel script
 #
+. /etc/functions
 . /etc/sysconfig/config
 if [ -e /initramfs/plugin-conf/Theme.conf ]; then
 	. /initramfs/plugin-conf/Theme.conf
@@ -13,7 +14,7 @@ if [ -e /initramfs/plugin-conf/Theme.conf ]; then
                 if [ -d /usr/share/themes/displaymanager ]; then
                         cp -a /usr/share/themes/displaymanager \
                           /mnt/var/lib/openslx/themes
-                        sed -i "s,UseTheme=false,UseTheme=true," /mnt/${kdmrcdir}/kdmrc
+                        sed -i "s,UseTheme=false,UseTheme=true," /mnt/${D_KDMRCPATH}/kdmrc
                 fi
 
 		[ $DEBUGLEVEL -gt 0 ] && echo "done with 'Theme' os-plugin ...";
