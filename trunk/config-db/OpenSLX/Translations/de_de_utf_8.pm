069e45670d50b79bfac93ba5594105de7a003467
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-21 01:58:43 +0000

    r442@pythagoras (orig r441):  zooey | 2006-10-04 15:41:41 +0200  * changed ODLX to OpenSLX such that things work again  * updated translatable strings with extractTranslations.pl, many translations are    still missing, though.

--- trunk/config-db/OpenSLX/Translations/de_de_utf_8.pm
+++ trunk/config-db/OpenSLX/Translations/de_de_utf_8.pm
@@ -4,7 +4,7 @@
 #
 # Oliver Tappe <ot@openslx.com>
 #
-package ODLX::Translations::de_de_utf_8;
+package OpenSLX::Translations::de_de_utf_8;
 
 use strict;
 use vars qw(@ISA @EXPORT $VERSION);
@@ -22,12 +22,151 @@ use vars qw(%translations);
 ################################################################################
 
 %translations = (
-	'Could not determine schema version of database'
-		=> 'Die Version des Datenbank-Schemas konnte nicht bestimmt werden',
-	'Unable to load DB-module <%s> (%s)'
-		=> 'Kann DB-Modul <%s> nicht laden (%s)',
-	'UnknownDbSchemaCommand'
-		=> 'Unbekannter DbSchema-Befehl <%s> wird übergangen',
+	qq{NEW:config-file <%s> has incorrect syntax here:\n\t%s\n}
+	=>
+	qq{},
+
+	qq{NEW:copying kernel %s to %s/kernel}
+	=>
+	qq{},
+
+	qq{Could not determine schema version of database}
+	=>
+	qq{Die Version des Datenbank-Schemas konnte nicht bestimmt werden},
+
+	qq{NEW:Could not load module <%s> (Version <%s> required, but <%s> found)}
+	=>
+	qq{},
+
+	qq{NEW:creating tar %s}
+	=>
+	qq{},
+
+	qq{NEW:DB matches current schema version %s}
+	=>
+	qq{},
+
+	qq{NEW:executing %s}
+	=>
+	qq{},
+
+	qq{NEW:exporting client %d:%s}
+	=>
+	qq{},
+
+	qq{NEW:exporting system %d:%s}
+	=>
+	qq{},
+
+	qq{NEW:generating initialramfs %s/initramfs}
+	=>
+	qq{},
+
+	qq{NEW:ignoring unknown key <%s>}
+	=>
+	qq{},
+
+	qq{NEW:merging %s (val=%s)}
+	=>
+	qq{},
+
+	qq{NEW:merging from default client...}
+	=>
+	qq{},
+
+	qq{NEW:merging from group %d:%s...}
+	=>
+	qq{},
+
+	qq{NEW:no}
+	=>
+	qq{},
+
+	qq{NEW:Our schema-version is %s, DB is %s, upgrading DB...}
+	=>
+	qq{},
+
+	qq{NEW:PXE-system %s already exists!}
+	=>
+	qq{},
+
+	qq{NEW:removing %s}
+	=>
+	qq{},
+
+	qq{NEW:setting %s to <%s>}
+	=>
+	qq{},
+
+	qq{NEW:system-error: illegal target-path <%s>!}
+	=>
+	qq{},
+
+	qq{NEW:translations module %s loaded successfully}
+	=>
+	qq{},
+
+	qq{NEW:Unable to access client-config-path '%s'!}
+	=>
+	qq{},
+
+	qq{NEW:Unable to create or access temp-path '%s'!}
+	=>
+	qq{},
+
+	qq{NEW:Unable to create or access tftpboot-path '%s'!}
+	=>
+	qq{},
+
+	qq{unable to execute shell-command:\n\t%s \n\t(%s)}
+	=>
+	qq{Konnte Shell-Kommando nicht ausführen:\n\t%s\n\t(%s)},
+
+	qq{Unable to load DB-module <%s> (%s)}
+	=>
+	qq{Kann DB-Modul <%s> nicht laden (%s)},
+
+	qq{NEW:Unable to load module <%s> (Version <%s> required)}
+	=>
+	qq{},
+
+	qq{NEW:Unable to load module <%s> (Version <%s> required, but <%s> found)}
+	=>
+	qq{},
+
+	qq{NEW:Unable to write local settings file <%s> (%s)}
+	=>
+	qq{},
+
+	qq{NEW:UnknownDbSchemaColumnDescr}
+	=>
+	qq{},
+
+	qq{UnknownDbSchemaCommand}
+	=>
+	qq{Unbekannter DbSchema-Befehl <%s> wird übergangen},
+
+	qq{NEW:UnknownDbSchemaTypeDescr}
+	=>
+	qq{},
+
+	qq{NEW:upgrade done}
+	=>
+	qq{},
+
+	qq{NEW:writing PXE-file %s}
+	=>
+	qq{},
+
+	qq{NEW:yes}
+	=>
+	qq{},
+
 );
 
 1;
+
+
+
+
+
--- trunk/config-db/OpenSLX/Translations/posix.pm
+++ trunk/config-db/OpenSLX/Translations/posix.pm
@@ -4,7 +4,7 @@
 #
 # Oliver Tappe <ot@openslx.com>
 #
-package ODLX::Translations::posix;
+package OpenSLX::Translations::posix;
 
 use strict;
 use vars qw(@ISA @EXPORT $VERSION);
@@ -22,18 +22,155 @@ use vars qw(%translations);
 ################################################################################
 
 %translations = (
-	'Could not determine schema version of database'
-		=> 'Could not determine schema version of database',
-	'Unable to load DB-module <%s> (%s)'
-		=> 'Unable to load DB-module <%s> (%s)',
-	'Unable to load module <%s> (Version <%s> required, but <%s> found)'
-		=> 'Unable to load module <%s> (Version <%s> required, but <%s> found)',
-	'UnknownDbSchemaCommand'
-		=> 'Unknown DbSchema command <%s> found',
-	'UnknownDbSchemaColumnDescr'
-		=> 'Unknown DbSchema column description <%s> found',
-	'UnknownDbSchemaTypeDescr'
-		=> 'Unknown DbSchema type description <%s> found',
+	qq{config-file <%s> has incorrect syntax here:\n\t%s\n}
+	=>
+	qq{config-file <%s> has incorrect syntax here:\n\t%s\n},
+
+	qq{copying kernel %s to %s/kernel}
+	=>
+	qq{copying kernel %s to %s/kernel},
+
+	qq{Could not determine schema version of database}
+	=>
+	qq{Could not determine schema version of database},
+
+	qq{Could not load module <%s> (Version <%s> required, but <%s> found)}
+	=>
+	qq{Could not load module <%s> (Version <%s> required, but <%s> found)},
+
+	qq{creating tar %s}
+	=>
+	qq{creating tar %s},
+
+	qq{DB matches current schema version %s}
+	=>
+	qq{DB matches current schema version %s},
+
+	qq{executing %s}
+	=>
+	qq{executing %s},
+
+	qq{exporting client %d:%s}
+	=>
+	qq{exporting client %d:%s},
+
+	qq{exporting system %d:%s}
+	=>
+	qq{exporting system %d:%s},
+
+	qq{generating initialramfs %s/initramfs}
+	=>
+	qq{generating initialramfs %s/initramfs},
+
+	qq{ignoring unknown key <%s>}
+	=>
+	qq{ignoring unknown key <%s>},
+
+	qq{merging %s (val=%s)}
+	=>
+	qq{merging %s (val=%s)},
+
+	qq{merging from default client...}
+	=>
+	qq{merging from default client...},
+
+	qq{merging from group %d:%s...}
+	=>
+	qq{merging from group %d:%s...},
+
+	qq{no}
+	=>
+	qq{no},
+
+	qq{Our schema-version is %s, DB is %s, upgrading DB...}
+	=>
+	qq{Our schema-version is %s, DB is %s, upgrading DB...},
+
+	qq{PXE-system %s already exists!}
+	=>
+	qq{PXE-system %s already exists!},
+
+	qq{removing %s}
+	=>
+	qq{removing %s},
+
+	qq{setting %s to <%s>}
+	=>
+	qq{setting %s to <%s>},
+
+	qq{system-error: illegal target-path <%s>!}
+	=>
+	qq{system-error: illegal target-path <%s>!},
+
+	qq{translations module %s loaded successfully}
+	=>
+	qq{translations module %s loaded successfully},
+
+	qq{Unable to access client-config-path '%s'!}
+	=>
+	qq{Unable to access client-config-path '%s'!},
+
+	qq{Unable to create or access temp-path '%s'!}
+	=>
+	qq{Unable to create or access temp-path '%s'!},
+
+	qq{Unable to create or access tftpboot-path '%s'!}
+	=>
+	qq{Unable to create or access tftpboot-path '%s'!},
+
+	qq{unable to execute shell-command:\n\t%s \n\t(%s)}
+	=>
+	qq{unable to execute shell-command:\n\t%s \n\t(%s)},
+
+	qq{Unable to load DB-module <%s> (%s)}
+	=>
+	qq{Unable to load DB-module <%s> (%s)},
+
+	qq{Unable to load module <%s> (Version <%s> required)}
+	=>
+	qq{Unable to load module <%s> (Version <%s> required)},
+
+	qq{Unable to load module <%s> (Version <%s> required, but <%s> found)}
+	=>
+	qq{Unable to load module <%s> (Version <%s> required, but <%s> found)},
+
+	qq{Unable to write local settings file <%s> (%s)}
+	=>
+	qq{Unable to write local settings file <%s> (%s)},
+
+	qq{UnknownDbSchemaColumnDescr}
+	=>
+	qq{Unknown DbSchema column description <%s> found},
+
+	qq{UnknownDbSchemaCommand}
+	=>
+	qq{Unknown DbSchema command <%s> found},
+
+	qq{UnknownDbSchemaTypeDescr}
+	=>
+	qq{Unknown DbSchema type description <%s> found},
+
+	qq{upgrade done}
+	=>
+	qq{upgrade done},
+
+	qq{writing PXE-file %s}
+	=>
+	qq{writing PXE-file %s},
+
+	qq{yes}
+	=>
+	qq{yes},
+
 );
 
+
+
+
+
 1;
+
+
+
+
+
