3ac420c0ea38132b1c3b22884cf1ba40b7de9463
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-21 23:57:18 +0000

    r1476@pythagoras (orig r1475):  zooey | 2008-01-14 21:38:46 +0100  * reactivated copying of displaymanager theme, but it still does not    seem to work, as all I see is the ugly standard look

--- trunk/os-plugins/plugins/Theme/XX_Theme.sh
+++ trunk/os-plugins/plugins/Theme/XX_Theme.sh
@@ -7,11 +7,11 @@ if [ -e /initramfs/plugin-conf/Theme.conf ]; then
 	if [ $Theme_active -ne 0 ]; then
 		[ $DEBUGLEVEL -gt 0 ] && echo "executing the 'Theme' os-plugin ...";
 	
-#		testmkd /mnt/var/lib/openslx/themes/displaymanager
-#		testmkd /mnt/var/lib/openslx/bin
-#		[ -d /usr/share/themes/displaymanager ] \
-#			&& cp -a /usr/share/themes/displaymanager \
-#			         /mnt/var/lib/openslx/themes
+		testmkd /mnt/var/lib/openslx/themes/displaymanager
+		testmkd /mnt/var/lib/openslx/bin
+		[ -d /usr/share/themes/displaymanager ] \
+			&& cp -a /usr/share/themes/displaymanager \
+			         /mnt/var/lib/openslx/themes
 
 		[ $DEBUGLEVEL -gt 0 ] && echo "done with 'Theme' os-plugin ...";
 	fi
