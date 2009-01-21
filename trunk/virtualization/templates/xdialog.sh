baf786d178b6dfd8f15ac3bdf1e9aede5b78eb71
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-21 23:40:38 +0000

    r1386@pythagoras (orig r1385):  uhrig | 2007-10-15 21:07:52 +0200  Sync with current system. Background themes added

--- trunk/virtualization/templates/xdialog.sh
+++ trunk/virtualization/templates/xdialog.sh
@@ -6,6 +6,17 @@
 #TODO: change path_to_this_script to path of this file. needed, because
 #      a return will exit Xdialog
 
+
+themesdir="/var/lib/openslx/themes"
+res=$(xvidtune -show | grep -wo "\".*\"" | sed "s/\"//g") 
+if ! [ -f ${themesdir}/bootsplash/images/silent-${res}.jpg ]; then     
+  img=$(ls ${themesdir}/bootsplash/images/ | grep -m 1 "silent")
+  display -window root ${themesdir}/bootsplash/images/${img}
+else
+  display -window root ${themesdir}/bootsplash/images/silent-${res}.jpg
+fi
+
+
 $($(which Xdialog) --rc-file /var/lib/openslx/themes/Xdialog/gtkrc \
     --title "Desktop / VMware-ImageMenu" \
     --screen-center \
