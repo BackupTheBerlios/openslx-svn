a2a3e6cf7efcb41dce92cc624c03d2b82db7e3be
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-22 15:44:47 +0000

    r1611@pythagoras (orig r1610):  schmelzs | 2008-03-07 19:27:57 +0100  * the plugin init script is now setting theme paths in gdm.conf

--- openslx/trunk/os-plugins/plugins/theme/XX_theme.sh
+++ openslx/trunk/os-plugins/plugins/theme/XX_theme.sh
@@ -16,6 +16,10 @@ if [ -e /initramfs/plugin-conf/theme.conf ]; then
                         cp -a /usr/share/themes/displaymanager \
                           /mnt/var/lib/openslx/themes
                         sed -i "s,UseTheme=false,UseTheme=true," /mnt/${D_KDMRCPATH}/kdmrc
+                        if [ -f /mnt/etc/gdm/gdm.conf ]; then
+			  sed -i "s,^\(GraphicalThemeDir=.*\)$,#\1 \nGraphicalThemeDir=/var/lib/openslx/themes/displaymanager," /mnt/etc/gdm/gdm.conf
+			  sed -i "s,^\(GraphicalTheme=.*\)$,#\1 \nGraphicalTheme=gdm," /mnt/etc/gdm/gdm.conf
+			fi
                 fi
 
 		[ $DEBUGLEVEL -gt 0 ] && echo "done with 'theme' os-plugin ...";
