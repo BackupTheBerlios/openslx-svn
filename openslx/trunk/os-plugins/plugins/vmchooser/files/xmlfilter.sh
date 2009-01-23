76e243a3d56ff4d47c5f40d7bfd75c4b3b0667a8
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 05:42:51 +0000

    r2451@pythagoras (orig r2450):  dvs | 2008-12-22 23:32:05 +0100  Just explained the use of the filter script: It allows to dynamically  display lists of vm sessions depending on the user just logged in, date,  time, ... (if re-scripted appropriately)

--- openslx/trunk/os-plugins/plugins/vmchooser/files/xmlfilter.sh
+++ openslx/trunk/os-plugins/plugins/vmchooser/files/xmlfilter.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Copyright (c) 2008 - Rechenzentrum Uni Freiburg, OpenSLX GmbH
 #
-# This script is a filter for the xml-files available
-# 
-# currently: 
+# This script simply filters xml-files (taking the path to these files in $1). # You might modify it in any way to match your needs, e.g. ask some database
+# instead. You can re-implement it in any other programming language too. You
+# simply have to return a list of proper xml files to be interpreted by the
+# vmchooser binary).
+#
+# currently:
 #     - filter for slxgrp (which comes from /etc/machine-setup)
 #
-
 
 if [ -f /etc/opt/openslx/vmchooser-stage3.conf ]; then
   . /etc/opt/openslx/vmchooser-stage3.conf
@@ -15,8 +18,8 @@ if [ -n ${vmchooser_env} ]; then
   for FILE in $1/*.xml
   do
     # filter all xmls with pool-param not equal to slxgroup
-    if [ `grep "<pools param=\".*${vmchooser_env}.*\"" $FILE | wc -l` -eq 1 ]; then
-      echo $FILE;
+    if [ $(grep "<pools param=\".*${vmchooser_env}.*\"" ${FILE} | wc -l) -eq 1 ]; then
+      echo ${FILE};
     fi
   done
 else
