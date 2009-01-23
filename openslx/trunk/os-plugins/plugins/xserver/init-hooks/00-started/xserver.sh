021dd813effbfa76c7bf1c26fdc1b07287c3c6fe
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 05:46:12 +0000

    r2471@pythagoras (orig r2470):  dvs | 2009-01-12 20:49:32 +0100  Addition, fix for tablet (wacom) detection ...

--- openslx/trunk/os-plugins/plugins/xserver/init-hooks/00-started/xserver.sh
+++ openslx/trunk/os-plugins/plugins/xserver/init-hooks/00-started/xserver.sh
@@ -1,3 +1,20 @@
+# Copyright (c) 2008 - RZ Uni Freiburg
+# Copyright (c) 2008 - OpenSLX GmbH
+#
+# This program/file is free software distributed under the GPL version 2.
+# See http://openslx.org/COPYING
+#
+# If you have any feedback please consult http://openslx.org/feedback and
+# send your feedback to feedback@openslx.org
+#
+# General information about OpenSLX can be found at http://openslx.org
+#
+# stage3 part of 'xserver' plugin - the runlevel script setting up the Xorg
+# configuration and checking for 3D capabilities and non-gpl drivers
+#
+# script is included from init via the "." load function - thus it has all
+# variables and functions available
+
 # get an idea of the installed graphics hardware - might be needed if the
 # automatic Xorg configation fails in this field. If no useable info was
 # detected just delete the file.
@@ -5,13 +22,14 @@
 # tablet detection function
 tabletdetect () {
   sleep 1; waitfor /etc/hwinfo.bios 20000
-  # quickhack for IBM X61 tablet detection
-  if grep -qi tablet /etc/hwinfo.bios ; then
+  # quickhack for IBM X61/ACER tablet detection (some kind of positive list
+  # or external admin configurable file needed)
+  if grep -qiE "tablet|TravelMate C200" /etc/hwinfo.bios ; then
     echo 'wacomdev="ttyS0"' >/etc/tablet.conf
   fi
   # wacom device attached to usb - code to be tested
   if [ ! -e /etc/tablet.conf ]; then
-    if hwinfo --usb | grep -qi tablet ; then
+    if hwinfo --usb | grep -qiE "wacom|tablet" ; then
       echo 'wacomdev="input/wacom"' >/etc/tablet.conf
     fi
   fi
