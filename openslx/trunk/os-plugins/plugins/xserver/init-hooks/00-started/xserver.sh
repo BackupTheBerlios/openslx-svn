6f641cd2544cd9170dc3bfb9d794a91b4ef3167a
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 04:19:07 +0000

    r2303@pythagoras (orig r2302):  dvs | 2008-10-15 00:22:44 +0200  Applying init-wrapper to the init-hook system.

--- openslx/trunk/os-plugins/plugins/xserver/init-hooks/00-started/xserver.sh
+++ openslx/trunk/os-plugins/plugins/xserver/init-hooks/00-started/xserver.sh
@@ -1,4 +1,3 @@
-#!/bin/ash
 # get an idea of the installed graphics hardware - might be needed if the
 # automatic Xorg configation fails in this field. If no useable info was
 # detected just delete the file.
