e8f8a32769d1e3768363bb11aa20dcf2438c57a2
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 01:31:14 +0000

    r1813@pythagoras (orig r1812):  zooey | 2008-05-29 21:40:18 +0200  * intermediate commit of refactoring step of MakeInitialRamFS - subversion    isn't as clever as I wish it'd be ...

---
+++ openslx/trunk/initramfs/OpenSLX/ImageBuilder/Distro/Ubuntu.pm
@@ -0,0 +1,47 @@
+# Copyright (c) 2006, 2007 - OpenSLX GmbH
+#
+# This program is free software distributed under the GPL version 2.
+# See http://openslx.org/COPYING
+#
+# If you have any feedback please consult http://openslx.org/feedback and
+# send your suggestions, praise, or complaints to feedback@openslx.org
+#
+# General information about OpenSLX can be found at http://openslx.org/
+# -----------------------------------------------------------------------------
+# ImageBuilder::Distro::Ubuntu.pm
+#    - provides Ubuntu-specific overrides of the ImageBuilder::Distro API.
+# -----------------------------------------------------------------------------
+package OpenSLX::ImageBuilder::Distro::Ubuntu;
+
+use strict;
+use warnings;
+
+use base qw(OpenSLX::ImageBuilder::Distro::Base);
+
+use OpenSLX::Basics;
+
+################################################################################
+### implementation
+################################################################################
+sub new
+{
+    my $class = shift;
+    my $self = {
+        'base-name' => 'ubuntu',
+    };
+    return bless $self, $class;
+}
+
+sub applyChanges
+{
+    my $self   = shift;
+    my $engine = shift;
+
+    $engine->_addFilteredKernelModules( qw( unix ));
+
+    $engine->_addRequiredLib('/lib/libnss_compat.so.2');
+
+    return;
+}
+
+1;
