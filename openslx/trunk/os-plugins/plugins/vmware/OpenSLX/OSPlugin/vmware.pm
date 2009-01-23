e5c68c873286e78a5d1d62bca5554e0822ad4a12
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 00:55:07 +0000

    r1636@pythagoras (orig r1635):  dvs | 2008-03-16 14:19:43 +0100  Changed to default indentation for vmware plugin files (two  spaces for shell scripts, four for perl). There are some discussions  to needed, how to handle vmware setup and configuration (the default  one by the software vendor is not really appropriate for the  environment here ...)

--- openslx/trunk/os-plugins/plugins/vmware/OpenSLX/OSPlugin/vmware.pm
+++ openslx/trunk/os-plugins/plugins/vmware/OpenSLX/OSPlugin/vmware.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2007 - OpenSLX GmbH
+# Copyright (c) 2008 - OpenSLX GmbH
 #
 # This program is free software distributed under the GPL version 2.
 # See http://openslx.org/COPYING
@@ -9,7 +9,7 @@
 # General information about OpenSLX can be found at http://openslx.org/
 # -----------------------------------------------------------------------------
 # vmware.pm
-#	- declares necessary information for the vmware plugin
+#    - declares necessary information for the vmware plugin
 # -----------------------------------------------------------------------------
 package OpenSLX::OSPlugin::vmware;
 
@@ -25,112 +25,112 @@ use OpenSLX::Utils;
 
 sub new
 {
-	my $class = shift;
+    my $class = shift;
 
-	my $self = {
-		name => 'vmware',
-	};
+    my $self = {
+        name => 'vmware',
+    };
 
-	return bless $self, $class;
+    return bless $self, $class;
 }
 
 sub getInfo
 {
-	my $self = shift;
+    my $self = shift;
 
-	return {
-		description => unshiftHereDoc(<<'		End-of-Here'),
-			!!! descriptive text missing here !!!
-		End-of-Here
-		mustRunAfter => [],
-	};
+    return {
+        description => unshiftHereDoc(<<'        End-of-Here'),
+            !!! descriptive text missing here !!!
+        End-of-Here
+        mustRunAfter => [],
+    };
 }
 
 sub getAttrInfo
-{	# returns a hash-ref with information about all attributes supported
-	# by this specific plugin
-	my $self = shift;
+{    # returns a hash-ref with information about all attributes supported
+    # by this specific plugin
+    my $self = shift;
 
-	# This default configuration will be added as attributes to the default
-	# system, such that it can be overruled for any specific system by means
-	# of slxconfig.
-	return {
-		# attribute 'active' is mandatory for all plugins
-		'vmware::active' => {
-			applies_to_systems => 1,
-			applies_to_clients => 0,
-			description => unshiftHereDoc(<<'			End-of-Here'),
-				should the 'vmware'-plugin be executed during boot?
-			End-of-Here
-			content_regex => qr{^(0|1)$},
-			content_descr => '1 means active - 0 means inactive',
-			default => '1',
-		},
-		# attribute 'precedence' is mandatory for all plugins
-		'vmware::precedence' => {
-			applies_to_systems => 1,
-			applies_to_clients => 0,
-			description => unshiftHereDoc(<<'			End-of-Here'),
-				the execution precedence of the 'vmware' plugin
-			End-of-Here
-			content_regex => qr{^\d\d$},
-			content_descr => 'allowed range is from 01-99',
-			default => '70',
-		},
-		# attribute 'imagesrc' defines where we can find vmware images
-		'vmware::imagessrc' => {
-			applies_to_systems => 1,
-			applies_to_clients => 1,
-			description => unshiftHereDoc(<<'			End-of-Here'),
-				Where do we store our vmware images? NFS? Filesystem?
-			End-of-Here
-			content_descr => 'Allowed values: path or URI',
-			default => '',
-		},
-		# attribute 'binaries' defines whether or not VMware binaries shall
-		# be provided (by downloading them).
-		'vmware::binaries' => {
-			applies_to_vendor_os => 1,
-			applies_to_systems => 0,
-			applies_to_clients => 0,
-			description => unshiftHereDoc(<<'			End-of-Here'),
-				Shall VMware binaries be downloaded and installed?
-			End-of-Here
-			content_regex => qr{^(0|1)$},
-			content_descr => 'Allowed values: 0 or 1',
-			default => '1',
-		},
+    # This default configuration will be added as attributes to the default
+    # system, such that it can be overruled for any specific system by means
+    # of slxconfig.
+    return {
+        # attribute 'active' is mandatory for all plugins
+        'vmware::active' => {
+            applies_to_systems => 1,
+            applies_to_clients => 0,
+            description => unshiftHereDoc(<<'            End-of-Here'),
+                should the 'vmware'-plugin be executed during boot?
+            End-of-Here
+            content_regex => qr{^(0|1)$},
+            content_descr => '1 means active - 0 means inactive',
+            default => '1',
+        },
+        # attribute 'precedence' is mandatory for all plugins
+        'vmware::precedence' => {
+            applies_to_systems => 1,
+            applies_to_clients => 0,
+            description => unshiftHereDoc(<<'            End-of-Here'),
+                the execution precedence of the 'vmware' plugin
+            End-of-Here
+            content_regex => qr{^\d\d$},
+            content_descr => 'allowed range is from 01-99',
+            default => '70',
+        },
+        # attribute 'imagesrc' defines where we can find vmware images
+        'vmware::imagessrc' => {
+            applies_to_systems => 1,
+            applies_to_clients => 1,
+            description => unshiftHereDoc(<<'            End-of-Here'),
+                Where do we store our vmware images? NFS? Filesystem?
+            End-of-Here
+            content_descr => 'Allowed values: path or URI',
+            default => '',
+        },
+        # attribute 'binaries' defines whether or not VMware binaries shall
+        # be provided (by downloading them).
+        'vmware::binaries' => {
+            applies_to_vendor_os => 1,
+            applies_to_systems => 0,
+            applies_to_clients => 0,
+            description => unshiftHereDoc(<<'            End-of-Here'),
+                Shall VMware binaries be downloaded and installed?
+            End-of-Here
+            content_regex => qr{^(0|1)$},
+            content_descr => 'Allowed values: 0 or 1',
+            default => '1',
+        },
 
-	};
+    };
 }
 
 sub installationPhase
 {
-	my $self                 = shift;
-	my $pluginRepositoryPath = shift;
-	my $pluginTempPath       = shift;
-	my $openslxPath          = shift;
+    my $self                 = shift;
+    my $pluginRepositoryPath = shift;
+    my $pluginTempPath       = shift;
+    my $openslxPath          = shift;
 
-	# get path of files we need to install
-	my $pluginFilesPath = "$openslxPath/lib/plugins/$self->{'name'}/files";
+    # get path of files we need to install
+    my $pluginFilesPath = "$openslxPath/lib/plugins/$self->{'name'}/files";
 
-	# copy all needed files now
-	my @files = qw( dhcpd.conf nat.conf nvram.5.0 runvmware-v2 );
-	foreach my $file (@files) {
-		copyFile("$pluginFilesPath/$file", $pluginRepositoryPath);
-	}
+    # copy all needed files now
+    my @files = qw( dhcpd.conf nat.conf nvram.5.0 runvmware-v2 );
+    foreach my $file (@files) {
+        copyFile("$pluginFilesPath/$file", $pluginRepositoryPath);
+    }
 }
 
 sub removalPhase
 {
-	my $self                 = shift;
-	my $pluginRepositoryPath = shift;
-	my $pluginTempPath       = shift;
-	my $openslxPath          = shift;
-	
-	rmtree ( [ $pluginRepositoryPath ] );
-	
-	return;
+    my $self                 = shift;
+    my $pluginRepositoryPath = shift;
+    my $pluginTempPath       = shift;
+    my $openslxPath          = shift;
+    
+    rmtree ( [ $pluginRepositoryPath ] );
+    
+    return;
 }
 
 1;
