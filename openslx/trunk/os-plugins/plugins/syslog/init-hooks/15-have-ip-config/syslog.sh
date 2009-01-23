88f8c1258a824ce416ad61cff80eadedb348d60f
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 01:05:46 +0000

    r1687@pythagoras (orig r1686):  zooey | 2008-03-24 20:15:30 +0100  * corrected indenting of 'syslog'-related shell scripts  * only try to remove boot.klog links if such an init script exists at all

--- openslx/trunk/os-plugins/plugins/syslog/init-hooks/15-have-ip-config/syslog.sh
+++ openslx/trunk/os-plugins/plugins/syslog/init-hooks/15-have-ip-config/syslog.sh
@@ -1,8 +1,8 @@
 if [ -e /initramfs/plugin-conf/syslog.conf ]; then
-    . /initramfs/plugin-conf/syslog.conf
-    if [ $syslog_active -ne 0 ] && [ -n "$syslog_host" ]; then
-        echo "syslogd -R ${syslog_host}:${syslog_port}..."
-        syslogd -R "${syslog_host}:${syslog_port}" & >/dev/null 2>&1
-        klogd >/dev/null 2>&1
-    fi
+  . /initramfs/plugin-conf/syslog.conf
+  if [ $syslog_active -ne 0 ] && [ -n "$syslog_host" ]; then
+    echo "syslogd -R ${syslog_host}:${syslog_port}..."
+    syslogd -R "${syslog_host}:${syslog_port}" & >/dev/null 2>&1
+    klogd >/dev/null 2>&1
+  fi
 fi
--- openslx/trunk/os-plugins/plugins/syslog/init-hooks/95-cleanup/syslog.sh
+++ openslx/trunk/os-plugins/plugins/syslog/init-hooks/95-cleanup/syslog.sh
@@ -1,10 +1,12 @@
 if [ -e /initramfs/plugin-conf/syslog.conf ]; then
-    . /initramfs/plugin-conf/syslog.conf
-    if [ $syslog_active -ne 0 ] && [ -n "$syslog_host" ]; then
-        # kill syslogd, as it is going to be replaced by system's syslog soon
-        killall syslogd
-        # remove links to boot.klog, as that will hang (I suppose that is 
-        # because we already emptied /dev/kmsg)
-        rm /mnt/etc/init.d/boot.d/*.klog
+  . /initramfs/plugin-conf/syslog.conf
+  if [ $syslog_active -ne 0 ] && [ -n "$syslog_host" ]; then
+    # kill syslogd, as it is going to be replaced by system's syslog soon
+    killall syslogd
+    # remove links to boot.klog, as that will hang (I suppose that is 
+    # because we already emptied /dev/kmsg)
+    if [ -e /mnt/etc/init.d/boot.klog ]; then
+      rm /mnt/etc/init.d/boot.d/*.klog
     fi
+  fi
 fi
