299962243742847560dac2b81774cce5f9858d53
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-01-23 03:02:38 +0000

    r2063@pythagoras (orig r2062):  zooey | 2008-08-20 18:26:06 +0200  * applied our cryptpw-blowfish patch

--- openslx-src-tools/trunk/busybox/loginutils/cryptpw.c
+++ openslx-src-tools/trunk/busybox/loginutils/cryptpw.c
@@ -34,19 +34,28 @@ done
 int cryptpw_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
 int cryptpw_main(int argc ATTRIBUTE_UNUSED, char **argv)
 {
-	char salt[sizeof("$N$XXXXXXXX")];
+    char salt[sizeof("$2a$10$1234567890123456789012X")];
 	char *opt_a;
 
-	if (!getopt32(argv, "a:", &opt_a) || opt_a[0] != 'd') {
+    if (!getopt32(argv, "a:", NULL)
+    || (argv[optind - 1][0] != 'b' && argv[optind - 1][0] != 'd')) {
+        /* md5 */
 		salt[0] = '$';
 		salt[1] = '1';
 		salt[2] = '$';
 		crypt_make_salt(salt + 3, 4, 0); /* md5 */
+#if ENABLE_FEATURE_CRYPTPW_BLOWFISH
+    } else if (argv[optind - 1][0] == 'b') {
+        /* blowfish */
+        strcpy(salt, "$2a$10$");
+        crypt_make_salt(salt + 7, 11, 0);
+#endif 
 #if TESTING
 		strcpy(salt + 3, "ajg./bcf");
 #endif
 	} else {
-		crypt_make_salt(salt, 1, 0);     /* des */
+		/* des */
+		crypt_make_salt(salt, 1, 0);
 #if TESTING
 		strcpy(salt, "a.");
 #endif
