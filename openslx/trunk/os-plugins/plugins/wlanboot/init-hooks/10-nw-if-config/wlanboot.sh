77c066b4af02027fbbc017a99dc2ba7ba4b76f27
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-05-28 14:38:16 +0000

    r5084@pythagoras (orig r2603):  dvs | 2009-02-18 18:34:53 +0100  Should have checked better (revoking parts of r2602).

--- openslx/trunk/os-plugins/plugins/wlanboot/init-hooks/10-nw-if-config/wlanboot.sh
+++ openslx/trunk/os-plugins/plugins/wlanboot/init-hooks/10-nw-if-config/wlanboot.sh
@@ -10,34 +10,26 @@
 #
 # stage3 init-hook 10 of 'wlanboot' plugin - firing up the wlan connection
 
-if [ -e /initramfs/plugin-conf/wlanboot.conf ] ; then
-  . /initramfs/plugin-conf/wlanboot.conf
-
-  if [ $wlanboot_active -ne 0 ]; then
-    # get essid for WLAN boot
-    for source in /proc/cmdline /etc/initramfs-setup ; do
-      essid=$(grep essid $source)
-      if [ -n "$essid" ] ; then
-        essid=${essid#essid=}
-        break
-      fi
-    done
-    # do WLAN specific settings, definition of wlan interface name and wireless
-    # connect
-    wlanif=$(iwconfig 2>/dev/null|sed -n "/ESSID:/p"|sed "s/    .*//")
-    if [ -n "$if" ] ; then
-      ip link set dev ${wlanif} up
-      if iwconfig ${wlanif} mode managed essid "${essid}"; then
-        nwif=${wlanif}
-      else
-        error "  Unable to configure the WLAN interface."
-      fi
-    else
-      error "  No wireless LAN capable interface found. Did you provide the \
-proper kernel\n  modules and firmware?"
-    fi
-  fi	
+# get essid for WLAN boot
+for source in /proc/cmdline /etc/initramfs-setup ; do
+  essid=$(grep essid $source)
+  if [ -n "$essid" ] ; then
+    essid=${essid#essid=}
+    break
+  fi
+done
+# do WLAN specific settings, definition of wlan interface name and wireless
+# connect
+wlanif=$(iwconfig 2>/dev/null|sed -n "/ESSID:/p"|sed "s/    .*//")
+if [ -n "$if" ] ; then
+  ip link set dev ${wlanif} up
+  if iwconfig ${wlanif} mode managed essid "${essid}"; then
+    nwif=${wlanif}
+  else
+    error "  Unable to configure the WLAN interface."
+  fi
 else
-  error "  No configuration file found for wlanboot plugin."
+  error "  No wireless LAN capable interface found. Did you provide the \
+proper kernel\n  modules and firmware?"
 fi
 
