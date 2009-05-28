ad2c427f0915acc511add9bb1fd77e643989cf9d
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-05-28 14:09:46 +0000

    r4991@pythagoras (orig r2510):  gasmi | 2009-01-20 18:48:20 +0100  new config.php

--- contrib/ldap-site-mngmt/webif/lib/config.inc.php
+++ contrib/ldap-site-mngmt/webif/lib/config.inc.php
@@ -1,55 +1,55 @@
 <?php
 
-	# URL and Root Path of LSM Webinterface
-	$START_PATH   = "http://your_host/path/";
-	$TITEL_PREFIX = "net&lowast;Client Management &nbsp; >> &nbsp; ";
+# -----------------------------------------------------
+# LSM LDAP Directory Service Settings
+# -----------------------------------------------------
+define('LDAP_HOST', 'ldap://<your_ldap_server>');
+define('LDAP_HOST', 'ldaps://<your_ldap_server>');
 
-	##################################
-	# LSM LDAP
+define('LDAP_PORT', 389);
+#define('LDAP_PORT', 636);
+$suffix = "dc=uni-freiburg,dc=de";
+# -----------------------------------------------------
+# External Authentification LDAP (optional)
+define('LDAP_HOST_EXT','ldaps://<external_auth_ldap_server>');
+define('LDAP_PORT_EXT', 636);
+$suffix_rz = "dc=uni-freiburg,dc=de";
 
-	#define('LDAP_HOST', 'ldap://your_ldap_server');
-	#define('LDAP_HOST', 'ldaps://your_ldap_server');	
-	define('LDAP_HOST', 'localhost');
+# -----------------------------------------------------
+# LSM Web-Interface Settings
+# -----------------------------------------------------
+# URL and Root Path of LSM Webinterface
+$START_PATH   = "http://your_host/path/";
+$TITEL_PREFIX = "net&lowast;Client Management &nbsp; >> &nbsp; ";
+$domsuffix    = "uni-freiburg.de";
+$rootAU       = "ou=UniFreiburg,ou=RIPM,dc=uni-freiburg,dc=de";
+# -----------------------------------------------------
+$dummyUid      = "lsmdummy";
+# Bitte nachfolgende Passwoerter aendern
+$dummyPassword = "123_dummy";
+$standpwd      = "123_user"
 
-	define('LDAP_PORT', 389);
-	#define('LDAP_PORT', 636);
+# -----------------------------------------------------
+# DHCP Service Global Settings
+$DHCP_SERVICE = "cn=DHCP_Uni,cn=dhcp,ou=UniFreiburg,ou=RIPM,dc=uni-freiburg,dc=de";
+$LEASE_TIMES  = array(
+	"600" => "10 min",
+	"900" => "15 min",
+	"1800"=> "30 min",
+	"3600" => "1 h",
+	"7200" => "2 h",
+	"18000" => "5 h",
+	"36000" => "10 h",
+	"86400" => "1 Tag",
+	"172800" => "2 Tage",
+	"345600" => "4 Tage",
+	"604800" => "1 Woche",
+	"1209600" => "2 Wochen"
+);
 
-	$suffix = "dc=uni-freiburg,dc=de";
-	
-	##################################
-	# External Authentification LDAP
-	define('LDAP_HOST_RZ','ldaps://bv1.ruf.uni-freiburg.de');
-        define('LDAP_PORT_RZ', 636);
-	$suffix_rz = "dc=uni-freiburg,dc=de";
-	
-	##################################
-	$domsuffix    = "uni-freiburg.de";
-	$rootAU       = "ou=UniFreiburg,ou=RIPM,dc=uni-freiburg,dc=de";
-	$DHCP_SERVICE = "cn=DHCP_Uni,cn=dhcp,ou=UniFreiburg,ou=RIPM,dc=uni-freiburg,dc=de";
-	$DNS_SERVICE  = "";
+# -----------------------------------------------------
+# Root Path to Addon DHCP Config Filestructure
+#$DHCP_FS_ROOT = "<path_to_external_filestructure>";
+$ADMIN_EMAIL  = "lsm-admin@mail-domain.xy";
 
-	# Root Path to Addon DHCP Config File Structure
-	$DHCP_FS_ROOT = "";	
-
-	$ADMIN_EMAIL  = "dhcp-admin@your_host";
-	###################################
-	
-	$LEASE_TIMES  = array("600" => "10 min",
-						  "900" => "15 min",
-						  "1800"=> "30 min",
-						  "3600" => "1 h",
-						  "7200" => "2 h",
-						  "18000" => "5 h",
-						  "36000" => "10 h",
-						  "86400" => "1 Tag",
-						  "172800" => "2 Tage",
-						  "345600" => "4 Tage",
-                                                  "604800" => "1 Woche",
-                                                  "1209600" => "2 Wochen");
-
-
-	$dummyUid      = "lsmdummy";
-	# Bitte nachfolgende Passwoerter aendern
-	$dummyPassword = "123_dummy";
-	$standpwd      = "123_user"
 ?>
