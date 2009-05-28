cfcea1857a3eed1e796dc120fa0fd76fc4341085
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-05-28 18:05:01 +0000

    r5305@pythagoras (orig r2824):  bw21 | 2009-04-23 15:19:40 +0200  commit of Makefile and basic Client-class.
     Set your build-Configuration to debug - i did not figure this out yet

---
+++ contrib/OekPoolFX/trunk/oekpool/Client.cpp
@@ -0,0 +1,17 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: 23.04.2009
+ *      Author: bastian
+ */
+
+#include "Client.h"
+
+Client::Client() {
+  // TODO Auto-generated constructor stub
+
+}
+
+Client::~Client() {
+  // TODO Auto-generated destructor stub
+}
---
+++ contrib/OekPoolFX/trunk/oekpool/main.cpp
@@ -0,0 +1,16 @@
+
+#include "main.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    cout << "Hello World!" << endl;
+
+    string bla = "Hello World!";
+
+
+    return true;
+}
