5de633bf9710149ba99e66b9ba4f865c652a88d6
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-06-04 13:48:03 +0000

    r5865@pythagoras (orig r2932):  schmelzs | 2009-06-04 15:26:19 +0200  fix "Module not found" bug of distroutils in plugin install routine

--- openslx/trunk/lib/OpenSLX/DistroUtils/Engine.pm
+++ openslx/trunk/lib/OpenSLX/DistroUtils/Engine.pm
@@ -29,17 +29,38 @@ sub new
 sub loadDistro {
     my $self = shift;
     my $distroName = shift;
+       $distroName = ucfirst($distroName);
     
     my $distro;
+    
+    my $pathToClass = "$openslxConfig{'base-path'}/lib";
+    my $flags = {};
+    if ($pathToClass) {
+        $flags->{incPaths} = [ $pathToClass ];
+        # if you call this function inside a plugin's install method we have to add /mnt
+        # in front of the include path!
+        $flags->{incPaths} = [ "/mnt/$pathToClass" ];
+    }
+    
     my $loaded = eval {
-            $distro = instantiateClass("OpenSLX::DistroUtils::${distroName}");
+            $distro = instantiateClass("OpenSLX::DistroUtils::${distroName}", $flags);
             return 0 if !$distro;   # module does not exist, try next
             1;
         };
+        
     if (!$loaded) {
-        $distro = instantiateClass("OpenSLX::DistroUtils::Base");
-        print ('couldnt load distro class');
+    	vlog(1, "can't find distro specific class, try base class..");
+        $loaded = eval {
+            $distro = instantiateClass("OpenSLX::DistroUtils::Base", $flags);
+            return 0 if !$distro;   # module does not exist, try next
+            1;
+        };
     }
+    
+    if (!$loaded) {
+        vlog(1, "failed to load DistroUtils!");
+    }
+
     return $distro;
 }
 
