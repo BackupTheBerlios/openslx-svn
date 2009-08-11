52b9adc43392eceff354a8de5f6ceb5643221242
ahoppe <ahoppe@281f87da-ed60-0410-8889-da28e8de9c04>
2009-08-11 10:49:17 +0000

    r6134@pythagoras (orig r3068):  dvs | 2009-08-11 12:17:46 +0200  Update of the syslog plugin ...

--- openslx/branches/5.0/os-plugins/plugins/syslog/OpenSLX/Distro/Base.pm
+++ openslx/branches/5.0/os-plugins/plugins/syslog/OpenSLX/Distro/Base.pm
@@ -18,6 +18,8 @@ use warnings;
 
 our $VERSION = 1.01;        # API-version . implementation-version
 
+use Scalar::Util qw( weaken );
+
 use OpenSLX::Basics;
 use OpenSLX::Utils;
 
@@ -35,6 +37,8 @@ sub initialize
 {
     my $self        = shift;
     $self->{engine} = shift;
+    weaken($self->{engine});
+        # avoid circular reference between plugin and its engine
     
     return 1;
 }
@@ -57,6 +61,18 @@ sub runlevelInfo
     };
 
     return $rlInfo;
+}
+
+sub getPackageName
+{
+    my $self = shift;
+    my $name = shift;
+
+    my %nameMap = (
+        'syslogd'   => 'sysklogd',
+        'syslog-ng' => 'syslog-ng',
+    );
+    return $nameMap{$name};
 }
 
 1;
